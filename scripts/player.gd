8d539dc82f77b2ba46f0ddf08d131086e44ff1e5
hyurielh <hyurielh@gmail.com>
2026-04-14 17:44:14 -0600

    another push

--- scripts/player.gd
+++ scripts/player.gd
@@ -17,8 +17,8 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		$AnimationPlayer.play("walk")
+		$AnimatedSprite2D.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$AnimationPlayer.stop()
+		
 	move_and_slide()
